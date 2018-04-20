8c2aa189a7b2cef761bbc8d3a2f19c304404ce32
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-04-20 16:24:16 -0300

    FEA: Reestruturação do client.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div id="app" class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8"><List/></div>
+        <div class="col-md-2"></div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+    import List from './components/List'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            List
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>Código (Code)</th>
+                    <th>Nome (Name)</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="country in countries.data">
+                    <td>{{country.code}}</td>
+                    <td>{{country.name}}</td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: 'countries',
+        data() {
+            return {
+                countries: [],
+            }
+        },
+        methods: {
+            loadCountries() {
+                axios.get(
+                    'http://localhost:8000/api/country',
+                    {
+                        headers: {
+                            'Access-Control-Allow-Origin': '*',
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                        }
+                    }
+                ).then((response) => {
+                    this.countries = response.data
+                }, (err) => {
+                    console.log(err)
+                })
+            }
+        },
+        mounted() {
+            this.loadCountries();
+        }
+    }
+</script>
+
+<style>
+</style>
